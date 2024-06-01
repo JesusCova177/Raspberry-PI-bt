b1b45ca85af873d32e91c0ab8f104a3110683dd5
dexst <dexst@VIERNES>
2024-06-01 01:39:23 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using InTheHand.Net.Sockets;
+using InTheHand.Net.Bluetooth;
+using System.Text;
+using InTheHand.Net;
+using System;
+
+class BluetoothClientExample
+{
+    public void SendData(string serverMacAddress, string data)
+    {
+        BluetoothClient client = new BluetoothClient();
+        BluetoothAddress addr = BluetoothAddress.Parse(serverMacAddress);
+        BluetoothEndPoint ep = new BluetoothEndPoint(addr, BluetoothService.SerialPort);
+
+        try
+        {
+            client.Connect(ep);
+            var stream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+            Console.WriteLine("Datos enviados: " + data);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al enviar datos: " + ex.Message);
+        }
+        finally
+        {
+            if (client.Connected)
+                client.Close();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BluetoothClientExample client = new BluetoothClientExample();
+        client.SendData("001122334455", "¡Hola desde Raspberry Pi!");
+    }
+}
